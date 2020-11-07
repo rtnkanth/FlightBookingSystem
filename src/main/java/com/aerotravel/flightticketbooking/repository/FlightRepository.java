45de261cd0de5fd1fd82252fc38d49a6c085d69f
rtnkanth <rtnkanth@gmail.com>
2020-11-07 14:20:56 +0530

    Spring Boot App

---
+++ src/main/java/com/aerotravel/flightticketbooking/repository/FlightRepository.java
@@ -0,0 +1,13 @@
+package com.aerotravel.flightticketbooking.repository;
+
+import com.aerotravel.flightticketbooking.model.Airport;
+import com.aerotravel.flightticketbooking.model.Flight;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.time.LocalDate;
+import java.util.List;
+
+public interface FlightRepository extends JpaRepository<Flight, Long> {
+    List<Flight> findAllByDepartureAirportEqualsAndDestinationAirportEqualsAndDepartureDateEquals(Airport depAirport, Airport destAirport, LocalDate depDate);
+
+}
